1e25084deb04fd85405aa9f038561ec3e1af9c3b
Kpexa <33284586+Kpexa@users.noreply.github.com>
2018-10-12 11:38:49 +0300

    commit

--- src/prejs/scripts.js
+++ src/prejs/scripts.js
@@ -86,140 +86,140 @@ $(function(){
 
   // MODAL WINDOW
 
-  let second_click = false;
-  let first_click_choise = {};
-  let modal_btn = $('.free-lesson .button #btn, .caption-upper-layer .button #btn');  
-  let choose_prog_btn = $('.choose-prog-btn');
-  let programm_confirm = $('.confirm');
-  let first_choise = $('.choise');  
+  // let second_click = false;
+  // let first_click_choise = {};
+  // let modal_btn = $('.free-lesson .button #btn, .caption-upper-layer .button #btn');  
+  // let choose_prog_btn = $('.choose-prog-btn');
+  // let programm_confirm = $('.confirm');
+  // let first_choise = $('.choise');  
 
-  modal_btn.magnificPopup().on('click', function(){
+  // modal_btn.magnificPopup().on('click', function(){
     
-    if(second_click === false) {
-      choose_prog_btn.on('click', function(e){
+  //   if(second_click === false) {
+  //     choose_prog_btn.on('click', function(e){
 
-        e.preventDefault();    
+  //       e.preventDefault();    
     
-        let user_name = $('.form input[name="name"]').val();
-        let user_age = $('.form input[name="age"]').val();
-        let user_weight = $('.form input[name="weight"]').val();
-        let user_gender = $('.form input[name="gender"]:checked').val();
-        let user_activities = getActivities();
-        let value_activities = getValueActivities();
-        let arr_activities = parseActivitiesValues(value_activities);       
+  //       let user_name = $('.form input[name="name"]').val();
+  //       let user_age = $('.form input[name="age"]').val();
+  //       let user_weight = $('.form input[name="weight"]').val();
+  //       let user_gender = $('.form input[name="gender"]:checked').val();
+  //       let user_activities = getActivities();
+  //       let value_activities = getValueActivities();
+  //       let arr_activities = parseActivitiesValues(value_activities);       
 
-        function getActivities (){
+  //       function getActivities (){
     
-          let activities = [];
+  //         let activities = [];
           
-          $('.form input:checkbox:checked').each(function(){            
-            activities.push($(this).data('activity'));
-          });
+  //         $('.form input:checkbox:checked').each(function(){            
+  //           activities.push($(this).data('activity'));
+  //         });
     
-          return activities;
-        }
+  //         return activities;
+  //       }
         
-        function getValueActivities(){
+  //       function getValueActivities(){
           
-          let value_activities = [];
+  //         let value_activities = [];
     
-          $('.form input:checkbox:checked').each(function(){
-            value_activities.push($(this).val());            
-          });
+  //         $('.form input:checkbox:checked').each(function(){
+  //           value_activities.push($(this).val());            
+  //         });
 
-          return value_activities
-        }
+  //         return value_activities
+  //       }
 
-        function parseActivitiesValues(values){
+  //       function parseActivitiesValues(values){
 
-          let arr = [];
+  //         let arr = [];
 
-          for(let i = 0; i < values.length; i++){
+  //         for(let i = 0; i < values.length; i++){
 
-            switch (values[i]){
-              case 'box':
-              arr.push($('.form input[name="activity-1"]').data('activity'));
-              break;
-              case 'crossfit':
-              arr.push($('.form input[name="activity-2"]').data('activity'));
-              break;
-              case 'powerlifting':
-              arr.push($('.form input[name="activity-3"]').data('activity'));
-              break;
-              case 'pilates':
-              arr.push($('.form input[name="activity-4"]').data('activity'));
-              break;
-              case 'aerobic':
-              arr.push($('.form input[name="activity-5"]').data('activity'));
-              break;
-            }
+  //           switch (values[i]){
+  //             case 'box':
+  //             arr.push($('.form input[name="activity-1"]').data('activity'));
+  //             break;
+  //             case 'crossfit':
+  //             arr.push($('.form input[name="activity-2"]').data('activity'));
+  //             break;
+  //             case 'powerlifting':
+  //             arr.push($('.form input[name="activity-3"]').data('activity'));
+  //             break;
+  //             case 'pilates':
+  //             arr.push($('.form input[name="activity-4"]').data('activity'));
+  //             break;
+  //             case 'aerobic':
+  //             arr.push($('.form input[name="activity-5"]').data('activity'));
+  //             break;
+  //           }
 
-          }
+  //         }
 
-          return arr;
-        }
+  //         return arr;
+  //       }
 
-        let programm_caption = `Уважаемый ${user_name}, вот список программ составленный
-                                исходя из Вашего выбора активностей`
+  //       let programm_caption = `Уважаемый ${user_name}, вот список программ составленный
+  //                               исходя из Вашего выбора активностей`
 
-        $('.programms').css('display', 'block');
-        $('.programms h3').html(programm_caption);
-        $('.programms-form').html(defineProgramm(user_activities));
+  //       $('.programms').css('display', 'block');
+  //       $('.programms h3').html(programm_caption);
+  //       $('.programms-form').html(defineProgramm(user_activities));
         
-      });
+  //     });
 
-      programm_confirm.on('click', function(){
-        $('.choise').css('display', 'block');
-        let choise_caption = `Уважаемый ${user_name}, вот список программ которые Вы выбрали ранее:`;
-        first_choise.html(choise_caption);
-        for(let i = 0; i < arr_activities.length; i++){
-          first_choise.html('<input type="checkbox" checked>' + arr_activities[i]);
-        }        
-      });     
+  //     programm_confirm.on('click', function(){
+  //       $('.choise').css('display', 'block');
+  //       let choise_caption = `Уважаемый ${user_name}, вот список программ которые Вы выбрали ранее:`;
+  //       first_choise.html(choise_caption);
+  //       for(let i = 0; i < arr_activities.length; i++){
+  //         first_choise.html('<input type="checkbox" checked>' + arr_activities[i]);
+  //       }        
+  //     });     
       
         
-        function defineProgramm (activities) {
+  //       function defineProgramm (activities) {
 
-          let programms;          
+  //         let programms;          
           
-          switch (activities.length){
-            case 5:
-            programms = `<input type="checkbox"> ${activities[0]}<br>
-                         <input type="checkbox"> ${activities[1]}<br>
-                         <input type="checkbox"> ${activities[2]}<br>
-                         <input type="checkbox"> ${activities[3]}<br>
-                         <input type="checkbox"> ${activities[4]}<br>`;
-            break;
-            case 4:
-            programms = `<input type="checkbox"> ${activities[0]}<br>
-                         <input type="checkbox"> ${activities[1]}<br>
-                         <input type="checkbox"> ${activities[2]}<br>
-                         <input type="checkbox"> ${activities[3]}<br>`;
-            break;
-            case 3:
-            programms = `<input type="checkbox"> ${activities[0]}<br>
-                         <input type="checkbox"> ${activities[1]}<br>
-                         <input type="checkbox"> ${activities[2]}<br>`;
-            break; 
-            case 2:
-            programms = `<input type="checkbox"> ${activities[0]}<br>
-                         <input type="checkbox"> ${activities[1]}<br>`;
-            break;
-            case 1:
-            programms = `<input type="checkbox"> ${activities[0]}<br>`;
-            break;               
-          }
+  //         switch (activities.length){
+  //           case 5:
+  //           programms = `<input type="checkbox"> ${activities[0]}<br>
+  //                        <input type="checkbox"> ${activities[1]}<br>
+  //                        <input type="checkbox"> ${activities[2]}<br>
+  //                        <input type="checkbox"> ${activities[3]}<br>
+  //                        <input type="checkbox"> ${activities[4]}<br>`;
+  //           break;
+  //           case 4:
+  //           programms = `<input type="checkbox"> ${activities[0]}<br>
+  //                        <input type="checkbox"> ${activities[1]}<br>
+  //                        <input type="checkbox"> ${activities[2]}<br>
+  //                        <input type="checkbox"> ${activities[3]}<br>`;
+  //           break;
+  //           case 3:
+  //           programms = `<input type="checkbox"> ${activities[0]}<br>
+  //                        <input type="checkbox"> ${activities[1]}<br>
+  //                        <input type="checkbox"> ${activities[2]}<br>`;
+  //           break; 
+  //           case 2:
+  //           programms = `<input type="checkbox"> ${activities[0]}<br>
+  //                        <input type="checkbox"> ${activities[1]}<br>`;
+  //           break;
+  //           case 1:
+  //           programms = `<input type="checkbox"> ${activities[0]}<br>`;
+  //           break;               
+  //         }
           
-          return programms;
+  //         return programms;
 
-      }  
+  //     }  
    
-    } else {
+  //   } else {
       
-    }   
+  //   }   
 
-    second_click = true    
-  });  
+  //   second_click = true    
+  // });  
   
 });
 
